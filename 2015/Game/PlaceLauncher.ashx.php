093a3198a08a23f6e368f7fea9305d724868b45e
minixalizied <119180434+epicminix@users.noreply.github.com>
2024-11-29 20:55:56 +0900

    Create PlaceLauncher.ashx.php

---
+++ 2015/Game/PlaceLauncher.ashx.php
@@ -0,0 +1,14 @@
+<?php
+$Waiting = "0";
+$Serverfound = "1";
+$joining = "2";
+?>
+{
+    "jobId":"join",
+    "status": 2,
+    "joinScriptUrl":"http://yourdomain.shouldbe10chars/Game/joinm",
+    "authenticationUrl":"http://yourdomain.shouldbe10chars/Login/Negotiate.ashx",
+    "authenticationTicket":null,
+    "message":null
+    
+}
