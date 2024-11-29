7e562c31b10e9eb488902dd624bc50fd1f824141
minixalizied <119180434+epicminix@users.noreply.github.com>
2024-11-29 21:11:03 +0900

    Create avatar-fetch.php

---
+++ 2015/v1.1/avatar-fetch.php
@@ -0,0 +1,11 @@
+<?php
+// Hello!
+// This is customizable avatar editor for your character
+// it should be giving for some ids specific assets that contain gears, clothes, hats that they equip
+// anyways, theres example for it
+// http://www.yourdomain.shouldbe10chars/asset/?id=1
+// you should put hats assets on your website because mobile doesn't like redirecting at all
+// Good Luck!
+?>
+
+http://www.yourdomain.shouldbe10chars/asset/BodyColors.ashx
