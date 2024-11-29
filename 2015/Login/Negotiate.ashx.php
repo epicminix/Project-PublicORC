5596c397c50a77e262dceab7e9ac9449be975f14
realestmunti <fle51@mail.ru>
2024-11-29 22:37:18 +0300

    Update Negotiate.ashx.php

--- 2015/Login/Negotiate.ashx.php
+++ 2015/Login/Negotiate.ashx.php
@@ -1,1 +1,11 @@
-True
+{
+	"Status": "OK",		
+	"UserInfo": {	
+		"UserName": "User",			
+		"RobuxBalance": 0,				
+		"TicketsBalance": 0,				
+		"IsAnyBuildersClubMember": false,				
+		"ThumbnailUrl": "",				
+		"UserID": 1	
+	}		
+}
